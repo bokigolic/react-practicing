642d326d453fdd1f943971baf2e7a6b20bd82bf1
Bojan Golic <bokigolic32@gmail.com>
2022-08-18 17:28:21 -0400

    dodato vise boja

--- src/components/KockicaGlupa.jsx
+++ src/components/KockicaGlupa.jsx
@@ -2,14 +2,20 @@ const KockicaGlupa = (props) => {
   const broj = props.broj;
 
   let color = "";
-  if (broj > 0 && broj < 4) {
+  if (broj > 0 && broj < 3) {
     color = "blue";
-  } else if (broj > 3 && broj < 7) {
+  } else if (broj > 2 && broj < 5) {
+    color = "green";
+  } else if (broj > 4 && broj < 7) {
     color = "yellow";
-  } else if (broj > 6 && broj < 10) {
+  } else if (broj > 6 && broj < 9) {
+    color = "orange";
+  } else if (broj > 8 && broj < 11) {
     color = "red";
-  } else if (broj > 9) {
+  } else if (broj > 10) {
     color = "red-hot";
+  } else if (broj < 0) {
+    color = "purple";
   }
 
   // <div className="kockica">
--- src/components/VezbeSaKockicama.jsx
+++ src/components/VezbeSaKockicama.jsx
@@ -14,15 +14,20 @@ const VezbeSaKockicama = () => {
       <KockicaPametna />
       <KockicaPametna />
       <KockicaPametna />
+      <KockicaGlupa broj={-5} />
+      <KockicaGlupa broj={1} />
       <KockicaGlupa broj={3} />
-      <KockicaGlupa broj={10} />
+      <KockicaGlupa broj={5} />
+      <KockicaGlupa broj={7} />
+      <KockicaGlupa broj={9} />
+      <KockicaGlupa broj={11} />
       <KockicaGlupa />
       <NizKockica niz={[4, 5, 8]} />
       <NizKockica niz={[1, 5, 9, 10, 12, 3]} />
       <NizKockica niz={[]} />
       <NizKockica niz={[1, 1]} />
       <MnogoKockica koliko={15} saBrojem={2} />
-      <MnogoKockica koliko={13} saBrojem={8} />
+      <MnogoKockica koliko={13} saBrojem={4} />
       <MatrixKockica kolikoRedova={3} kolikoKolona={4} saBrojem={5} />
       <MatrixKockica kolikoRedova={4} kolikoKolona={2} saBrojem={7} />
       <MatrixKockica kolikoRedova={5} kolikoKolona={5} saBrojem={1} />
