fa33f73483419b846113bc1935642d5d5ac1f5c3
Bojan Golic <bokigolic32@gmail.com>
2022-08-22 15:50:28 -0400

    univerzalni calculator buttons

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,3 +1,5 @@
+import CalculatorBtn from "./CalculatorBtn";
+
 const Calculator = () => {
   return (
     <>
@@ -7,12 +9,25 @@ const Calculator = () => {
           123
         </div>
         <div className="calculator-tastatura">
-          <div className="calculator-taster">8</div>
-          <div className="calculator-taster">8</div>
-          <div className="calculator-taster">8</div>
-          <div className="calculator-taster">8</div>
-          <div className="calculator-taster">8</div>
-          <div className="calculator-taster">8</div>
+          <CalculatorBtn title={7} />
+          <CalculatorBtn title={8} />
+          <CalculatorBtn title={9} />
+          <CalculatorBtn title={<span>&times;</span>} />
+
+          <CalculatorBtn title={4} />
+          <CalculatorBtn title={5} />
+          <CalculatorBtn title={6} />
+          <CalculatorBtn title={<span>-</span>} />
+
+          <CalculatorBtn title={1} />
+          <CalculatorBtn title={2} />
+          <CalculatorBtn title={3} />
+          <CalculatorBtn title={<span>+</span>} />
+
+          <CalculatorBtn title={''} />
+          <CalculatorBtn title={0} />
+          <CalculatorBtn title={'.'} />
+          <CalculatorBtn title={<span>=</span>} />
         </div>
 
       </div>
---
+++ src/components/CalculatorBtn.jsx
@@ -0,0 +1,7 @@
+const CalculatorBtn = (props) => {
+  return (
+    <div className="calculator-taster">{props.title}</div>
+  )
+};
+
+export default CalculatorBtn;
