2369a54854e1a42534fa87969887ac14378c8f24
bokigolic <bokigolic32@gmail.com>
2022-08-29 14:21:02 -0400

    dodali smo prosecnu i zakljucnu ocenu

--- src/components/SkolskiDnevnik.jsx
+++ src/components/SkolskiDnevnik.jsx
@@ -1,11 +1,47 @@
 import { useState } from "react";
 
+
+// utils
+const izracunajProsecnuOcenu = (nizOcena) => {
+  let brojac = 0;
+  let zbir = 0;
+  nizOcena.forEach((ocena) => {
+    brojac++;
+    zbir += ocena;
+  });
+  if (brojac === 0) {
+    // ne sme da se deli sa nolom
+    return 0;
+  }
+  const prosek = zbir / brojac; // prosek decfimalni broj na priemr 3.25
+  const prosecnaOcena = Math.round(prosek); // prosecna ocena na primer 3
+  return prosecnaOcena;
+};
+
+const daLiJePao = (nizOcena) => {
+  let pao = false; //prosao a polse toga moze sam oda padne
+  nizOcena.forEach((ocena) => {
+    if (ocena === 1) {
+      // ako ima bar jednu jedinicu pao je bez obzira na ostale ocene
+      pao = true;
+    }
+    if (ocena === 0) {
+      // ako je neocenjen iz nekog predmeta takodje pada
+      pao = true;
+    }
+  });
+  return pao;
+};
+
+
+
+// komponenta
 const SkolskiDnevnik = () => {
 
   const initialOcene = {
     biologija: [3],
     matematika: [2],
-    srpski: [4, 5, 1],
+    srpski: [4, 4, 2, 1],
     istorija: [1],
     geografija: [2, 4]
   };
@@ -15,6 +51,20 @@ const SkolskiDnevnik = () => {
 
   const [ocene, setOcene] = useState(initialOcene);
 
+
+  let prosecneOceneIzSvihPredmeta = [];
+  Object.keys(ocene).forEach((predmet) => {
+    const oceneIzTogPredmeta = ocene[predmet];
+    const prosecnaOcena = izracunajProsecnuOcenu(oceneIzTogPredmeta);
+    prosecneOceneIzSvihPredmeta.push(prosecnaOcena);
+  });
+
+  const glavnaProsecnaOcena = izracunajProsecnuOcenu(prosecneOceneIzSvihPredmeta)
+
+
+
+  let pao = daLiJePao(prosecneOceneIzSvihPredmeta);
+
   return (
     <div className="skolski-dnevnik">
       <h1>Skolski Dnevnik</h1>
@@ -22,11 +72,12 @@ const SkolskiDnevnik = () => {
       {
         Object.keys(ocene).map((predmet) => {
           const oceneIzTogPredmeta = ocene[predmet];
-          const oceneZaIspis = oceneIzTogPredmeta.join(', ')
+          const oceneZaIspis = oceneIzTogPredmeta.join(', ');
+          const prosecnaOcena = izracunajProsecnuOcenu(oceneIzTogPredmeta);
           return (
             <div>
               <h4>{predmet}</h4>
-              <p>Ocene: {oceneZaIspis}</p>
+              <p>Ocene: {oceneZaIspis} - prosečna: {prosecnaOcena}</p>
               <div>
                 Daj ocenu: <button>1</button>
                 <button>2</button>
@@ -40,7 +91,15 @@ const SkolskiDnevnik = () => {
       }
 
 
-      <h2>Prosecna ocena: </h2>
+      <h2>Prosecna ocena iz svih predmeta: <b>{glavnaProsecnaOcena}</b></h2>
+
+      {
+        pao ? (
+          <div>Pao (iz jednog predmeta imate jedinicu ili neocenjen predmet)!</div>
+        ) : (
+          <div>Prolazna ocena :) Cestitamo!</div>
+        )
+      }
     </div>
 
   )
