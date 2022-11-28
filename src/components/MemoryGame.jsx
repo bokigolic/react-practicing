586e53ef95bfb4cc627e07ae2604ee94c1679910
bokigolic <bokigolic32@gmail.com>
2022-11-28 15:34:01 -0500

    improved names of players

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -43,6 +43,21 @@ const MemoryGame = () => {
     usedTime: 0,
     score: 0, // nuber of pairs
   }
+  const initialPlayer1 = {
+    name: 'Bravo',
+    usedTime: 0,
+    score: 0, // nuber of pairs
+  }
+  const initialPlayer2 = {
+    name: 'Charlie',
+    usedTime: 0,
+    score: 0, // nuber of pairs
+  }
+  const initialPlayer3 = {
+    name: 'Delta',
+    usedTime: 0,
+    score: 0, // nuber of pairs
+  }
   const [players, setPlayers] = useState([initialPlayer]);
   const [howManyPlayers, setHowManyPlayers] = useState(1)
   const [size, setSize] = useState(16);
@@ -138,11 +153,11 @@ const MemoryGame = () => {
     if (howManyPlayers === 1) {
       setPlayers([initialPlayer]);
     } else if (howManyPlayers === 2) {
-      setPlayers([initialPlayer, initialPlayer])
+      setPlayers([initialPlayer, initialPlayer1])
     } else if (howManyPlayers === 3) {
-      setPlayers([initialPlayer, initialPlayer, initialPlayer])
+      setPlayers([initialPlayer, initialPlayer1, initialPlayer2])
     } else if (howManyPlayers === 4) {
-      setPlayers([initialPlayer, initialPlayer, initialPlayer, initialPlayer])
+      setPlayers([initialPlayer, initialPlayer1, initialPlayer2, initialPlayer3])
     }
     setStarted(true) // igra je zapoceta
     // TODO zapocinjemo merenje vremena za igraca
@@ -166,6 +181,11 @@ const MemoryGame = () => {
     }
   }
 
+
+  // pripremamo od trenutnog igraca
+  const currentPlayerData = players[currentPlayer];
+  const currentPlayerName = currentPlayerData.name;
+
   // adaptiramo sirinu table za igru na broj kockica
   let width = 4 * 70; // 4 columns default
   if (size === 36 || size === 24) {
@@ -179,7 +199,7 @@ const MemoryGame = () => {
   return (
     <div>
       <h2>Memory Game</h2>
-      <p>Player {currentPlayer} make your move</p>
+      <p>Player {currentPlayerName} make your move</p>
       <div
         className="memory-board"
         style={{
@@ -218,7 +238,7 @@ const MemoryGame = () => {
                 // const displayTime = miliSecondsToDisplayFormat(player.usedTime)
                 const displayTime = miliSecondsToLongTimeWithoutHours(player.usedTime);
                 let isCurrentPlayer = false;
-                if(index === currentPlayer){
+                if (index === currentPlayer) {
                   isCurrentPlayer = true;
                 }
                 return (
